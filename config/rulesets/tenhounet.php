92f9ff84a177a301b8e3b24def31db5e5df163a7
Oleg Klimenko <o.klimenko@2gis.ru>
2016-09-30 21:11:24 +0700

    Basic rulesets for popular rules

---
+++ config/rulesets/tenhounet.php
@@ -0,0 +1,65 @@
+<?php
+/*  Riichi mahjong API game server
+ *  Copyright (C) 2016  heilage and others
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+namespace Riichi;
+
+require_once __DIR__ . '/../../src/Ruleset.php';
+
+/**
+ * Class RulesetTenhounet
+ * Describes most popular row3-column2 rules
+ * @package Riichi
+ */
+class RulesetTenhounet extends Ruleset
+{
+    protected static $_title = 'tenhounet';
+    protected static $_ruleset = [
+        'tenboDivider'          => 1000,
+        'ratingDivider'         => 1,
+        'startRating'           => 1500,
+        'oka'                   => 20,
+        'startPoints'           => 25,
+        'riichiGoesToWinner'    => false,
+        'extraChomboPayments'   => true,
+        'chomboPenalty'         => 0,
+        'withAtamahane'         => false,
+        'withAbortives'         => true,
+        'withKuitan'            => true,
+        'withKazoe'             => true,
+        'withButtobi'           => true,
+        'withMultiYakumans'     => true,
+        'withOpenRiichi'        => false,
+        'withNagashiMangan'     => true,
+        'tonpuusen'             => false,
+        'withLeadingDealerGameOver' => true,
+        'uma' => [
+            1 => 15,
+            2 => 5,
+            3 => -5,
+            4 => -15
+        ],
+    ];
+
+    public function calcRating($currentRating, $place, $points)
+    {
+        return $currentRating + (
+            ($points + $this->uma()[$place]) / (float)$this->ratingDivider()
+        );
+    }
+}
+
+return RulesetTenhounet::instance();
