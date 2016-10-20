62bd1a52f27fd3234b9bbb58c26dfdf1d3f3a88f
Oleg Klimenko <o.klimenko@2gis.ru>
2016-10-20 21:02:58 +0700

    Replace name in license & add dist info

--- src/controllers/Sortition.php
+++ src/controllers/Sortition.php
@@ -1,6 +1,6 @@
 <?php
 /*  Riichi mahjong API game server
- *  Copyright (C) 2016  heilage and others
+ *  Copyright (C) 2016  o.klimenko aka ctizen
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
