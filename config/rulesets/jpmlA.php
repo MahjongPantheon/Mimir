0a007c1be939affc3a58f97f3af59882593f2361
Oleg Klimenko <o.klimenko@2gis.ru>
2017-03-01 10:27:04 +0700

    JPML A: disable agariyame/tempaiyame

--- config/rulesets/jpmlA.php
+++ config/rulesets/jpmlA.php
@@ -45,7 +45,7 @@ class RulesetJpmlA extends Ruleset
         'minPenalty'            => 10,
         'maxPenalty'            => 200,
         'penaltyStep'           => 10,
-        'withLeadingDealerGameOver' => true
+        'withLeadingDealerGameOver' => false
     ];
 
     public function allowedYaku()
